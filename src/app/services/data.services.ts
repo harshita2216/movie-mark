f02689edcc60d82f0369af061bc449cdf3ef8c0b
harshita2216 <harshita2216@gmail.com>
2021-03-22 22:39:09 +0530

    mixed content error fixed

--- src/app/services/data.services.ts
+++ src/app/services/data.services.ts
@@ -2,7 +2,7 @@
 export class DataServices { 
 
     findMovieByTitle(title) {
-        const url = `http://www.omdbapi.com/?s=${title}&apikey=453b231`;
+        const url = `https://www.omdbapi.com/?s=${title}&apikey=453b231`;
         return fetch(url)
         .then(res => res.json())
         .catch(err =>  console.log("Error! :D", err))
